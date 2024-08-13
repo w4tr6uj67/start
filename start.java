b1ee6056dbb9a84f36da84c7206c2c50c1eef816
w4tr6uj67 <ThakorBhoyar542@gmail.com>
2024-08-13 18:22:02 +0800

    Create start.java

---
+++ start.java
@@ -0,0 +1,47 @@
+/* 实例化对象 */
+Object referenceVariable = new Constructor();
+/* 访问类中的变量 */
+referenceVariable.variableName;
+/* 访问类中的方法 */
+referenceVariable.methodName();
+public class Puppy {
+    private int age;
+    private String name;
+ 
+    // 构造器
+    public Puppy(String name) {
+        this.name = name;
+        System.out.println("小狗的名字是 : " + name);
+    }
+ 
+    // 设置 age 的值
+    public void setAge(int age) {
+        this.age = age;
+    }
+ 
+    // 获取 age 的值
+    public int getAge() {
+        return age;
+    }
+ 
+    // 获取 name 的值
+    public String getName() {
+        return name;
+    }
+ 
+    // 主方法
+    public static void main(String[] args) {
+        // 创建对象
+        Puppy myPuppy = new Puppy("Tommy");
+ 
+        // 通过方法来设定 age
+        myPuppy.setAge(2);
+ 
+        // 调用另一个方法获取 age
+        int age = myPuppy.getAge();
+        System.out.println("小狗的年龄为 : " + age);
+ 
+        // 也可以直接访问成员变量（通过 getter 方法）
+        System.out.println("变量值 : " + myPuppy.getAge());
+    }
+}
